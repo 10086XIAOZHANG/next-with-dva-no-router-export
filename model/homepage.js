f55ec4162eeb0aa43dbf84eee25c3da982aac08b
jimck <zhangjun1@onething.net>
2018-10-29 17:38:08 +0800

    修改

--- model/homepage.js
+++ model/homepage.js
@@ -3,21 +3,32 @@ const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 const model = {
     namespace: 'index',
     state: {
-        name: 'hopperhuang',
+        name: 'jimck',
         count: 0,
         init: false,
     },
     reducers: {
         caculate(state, payload) {
-            const { count } = state;
-            const { delta } = payload;
-            return { ...state, count: count + delta };
+            const {
+                count
+            } = state;
+            const {
+                delta
+            } = payload;
+            return { ...state,
+                count: count + delta
+            };
         },
     },
     effects: {
-        *init(action, { put }) {
+        * init(action, {
+            put
+        }) {
             yield delay(2000);
-            yield put({ type: 'caculate', delta: 1 });
+            yield put({
+                type: 'caculate',
+                delta: 1
+            });
         },
     },
 };
